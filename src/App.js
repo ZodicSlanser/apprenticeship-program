7dbfeb92881b59f64f88651feadd3cad4a832bcb
Amr Mohamed <amrmohamedyonis@gmail.com>
2022-10-02 21:31:02 +0200

    modified ProgressBar to fit ApprenticeShip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,104 @@
+import ProgressBar from "./Components/ProgressBar";
 function App() {
-  return;
+  let invokeActivitySetter;
+  const invokeActivity = (setStateCallback, active) => {
+    if (setStateCallback) {
+      invokeActivitySetter = setStateCallback[1];
+    } else {
+      invokeActivitySetter(active);
+    }
+  };
+  let invokeDescriptionSetter;
+  const invokeDescription = (setStateCallback, done) => {
+    if (setStateCallback) {
+      invokeDescriptionSetter = setStateCallback[1];
+    } else {
+      invokeDescriptionSetter(done);
+    }
+  };
+  let invokeTypeSetter;
+  const invokeType = (setStateCallback, done) => {
+    if (setStateCallback) {
+      invokeTypeSetter = setStateCallback[1];
+    } else {
+      invokeTypeSetter(done);
+    }
+  };
+  let invokeRolesSetter;
+  const invokeRoles = (setStateCallback, done) => {
+    if (setStateCallback) {
+      invokeRolesSetter = setStateCallback[1];
+    } else {
+      invokeRolesSetter(done);
+    }
+  };
+  let invokeAdminSetter;
+  const invokeAdmin = (setStateCallback, done) => {
+    if (setStateCallback) {
+      invokeAdminSetter = setStateCallback[1];
+    } else {
+      invokeAdminSetter(done);
+    }
+  };
+  let invokeTimelineSetter;
+  const invokeTimeline = (setStateCallback, done) => {
+    if (setStateCallback) {
+      invokeTimelineSetter = setStateCallback[1];
+    } else {
+      invokeTimelineSetter(done);
+    }
+  };
+  return (
+    <>
+      <ProgressBar
+        invokeActivity={invokeActivity}
+        invokeDescription={invokeDescription}
+        invokeType={invokeType}
+        invokeRoles={invokeRoles}
+        invokeAdmin={invokeAdmin}
+        invokeTimeline={invokeTimeline}
+      ></ProgressBar>
+      <div
+        style={{ width: "600px", height: "100px", backgroundColor: "red" }}
+        onClick={() => invokeActivity(null, 1)}
+        onBlur={() => invokeActivity(null, 0)}
+        tabindex="-1"
+      >
+        Description
+      </div>
+      <div
+        style={{ width: "600px", height: "100px", backgroundColor: "blue" }}
+        onClick={() => invokeActivity(null, 2)}
+        onBlur={() => invokeActivity(null, 0)}
+        tabindex="-1"
+      >
+        Type
+      </div>
+      <div
+        style={{ width: "600px", height: "100px", backgroundColor: "green" }}
+        onClick={() => invokeActivity(null, 3)}
+        onBlur={() => invokeActivity(null, 0)}
+        tabindex="-1"
+      >
+        Roles
+      </div>
+      <div
+        style={{ width: "600px", height: "100px", backgroundColor: "yellow" }}
+        onClick={() => invokeActivity(null, 4)}
+        onBlur={() => invokeActivity(null, 0)}
+        tabindex="-1"
+      >
+        Admin
+      </div>
+      <div
+        style={{ width: "600px", height: "100px", backgroundColor: "grey" }}
+        onClick={() => invokeActivity(null, 5)}
+        onBlur={() => invokeActivity(null, 0)}
+        tabindex="-1"
+      >
+        Timeline
+      </div>
+    </>
+  );
 }
 export default App;

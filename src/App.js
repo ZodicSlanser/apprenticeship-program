c65942e6be269ae68110c16da4c4451e18a9a879
rroshdy11 <rroshdy75@gmail.com>
2022-10-05 23:46:55 +0200

    Empty Panel

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import TeamRoles from './Components/TeamRoles';
+
+function App() {
+    return ( 
+      <>
+      <TeamRoles />
+    </>
+    )
+}
+export default App;
---
+++ src/Components/TeamRoles.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import  '../styles/TeamRoles.css'
+import addCircle from '../icons/add-circle.png'
+import infoCircle from '../icons/info-circle.png'
+export default function TeamRoles(props) {
+    return (
+        <section className="team-roles-panel">
+           <div className='team-Button-a'>
+            <div><p>Team Roles</p>
+            <button className="team-roles-button">
+                <img src={addCircle} alt='add Circle Icon' className='add-circle-icon'/>
+                Add Team Member
+            </button></div>
+            <img src={infoCircle} alt='Info Circle Icon' className='info-circle-icon'></img>
+           </div>
+           <div className='available-roles'></div>
+        </section>
+
+    )
+}

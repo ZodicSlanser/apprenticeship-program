5f0dc8fde267f76ad4459ca82efee365848953ac
NourineAbouElWafa <nourineabouelwafa14@gmail.com>
2022-10-07 22:36:06 +0200

    team-admin with tags

--- src/Components/TeamAdmin.jsx
+++ src/Components/TeamAdmin.jsx
@@ -1,33 +1,87 @@
-import React,{ useState }from 'react';
+import React, { useState } from 'react';
 import "../styles.css";
 import TeamPage from "./TeamPage"
+import Rectangle from "./Rectangle.svg";
 import addCircle from "./add-circle.svg"
 import infoCircle from "./info-circle.svg"
-function  TeamAdmin(){
+import link from "./LinkedIn logo.svg"
+
+function TeamAdmin() {
     const [modalOpen, setModal] = useState(false);
-    return(
-        
+    const [selectedImage, setSelectedImage] = useState();
+    const [formData, setFormData] = useState(
+        { firstName: '', url: '', photo: '' }
+    )
+    const [list, setList] = useState([])
+
+
+    console.log(formData)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setList((ls) => [...ls, formData])
+        setFormData('')
+    }
+
+    const handleImage = (e) => {
+        if (e.target.files && e.target.files.length > 0) {
+            setSelectedImage(e.target.files[0]);
+            setFormData(prevFormData => {
+                return {
+                    ...prevFormData,
+                    [e.target.name]: URL.createObjectURL(e.target.files[0])
+                }
+            })
+
+        }
+    }
+
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+    return (
+
         <div className='Team-container'>
-        <div className='Title-container'>
-        <div className='Title'>
-            <span>Team Admin</span>
-            <button className='Add-member' onClick={() => {
-          setModal(true);
-        }}>
-                <img src={addCircle}></img>
-                <span> Add Team Member</span>
-            </button>
-        </div>
-        <img src={infoCircle} className='info-circle'></img>
-        </div>
-        <div className='TeamAdmin-container'>
-            <div className='Admin-details'>
-                
+            <div className='Title-container'>
+                <div className='Title'>
+                    <span>Team Admin</span>
+                    <button className='Add-member' onClick={() => {
+                        setModal(true);
+                    }}>
+                        <img src={addCircle}></img>
+                        <span> Add Team Member</span>
+                    </button>
+                </div>
+                <img src={infoCircle} className='info-circle'></img>
             </div>
+            <div className='TeamAdmin-container' >
+                {list.map((a, index) => (
+
+                    <div className='Admin-details' key={index}>
+                        {selectedImage ? <img src={a.photo}></img> : null}
+                        <div className='admin-firstName'>
+                            <span> {a.firstName}</span>
+
+                        </div>
+                        <a href={a.url}><img src={link}></img></a>
+
+
+                    </div>
+
+
+
+
+                ))}
+            </div>
+
+            {modalOpen && <TeamPage closeModal={() => setModal(false)} handleform={handleChange}
+                selectedImage={selectedImage} changeImage={handleImage} submit={handleSubmit} />}
         </div>
-        {modalOpen && <TeamPage  closeModal={()=> setModal(false)}/>}
-        </div>
-        
+
     )
 }
 export default TeamAdmin;
--- src/Components/TeamPage.jsx
+++ src/Components/TeamPage.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useRef}from 'react';
+import React, { useState, useRef } from 'react';
 import "../styles.css";
 import close from "./close.svg";
 import profile from "./profile.svg";
@@ -7,78 +7,63 @@ import link from "./link.svg";
 import Rectangle from "./Rectangle.svg";
 import upload from "./upload.svg";
 
-function TeamPage( { closeModal }){
-    const [selectedImage, setSelectedImage] = useState();
-    const fileInputRef = useRef();
-    const inputName = useRef(null);
-    const inputUrl = useRef(null);
- 
+function TeamPage(props) {
+  const fileInputRef = useRef();
 
-      const handleImage = (e) =>{
-    if (e.target.files && e.target.files.length > 0) {
-        setSelectedImage(e.target.files[0]);
-      }
-  }
-  function handleClick() {
-    console.log(inputName.current.value);
-    console.log(inputUrl.current.value);
-    console.log(URL.createObjectURL(selectedImage));
+  return (
+    <div className='popup-container' >
+      <div className='popup'>
+        <div className='popup-header'>
+          <span>
+            Add Team Admin
+          </span>
+          <div className='popup-btns'>
+            <button className='save-btn' onClick={props.submit}>
+              <span> Save</span>
+            </button>
 
-  }
-    return(
-        <div className='popup-container' >
-        <div className='popup'>
-            <div className='popup-header'>
-                <span>
-                    Add Team Admin
-                </span>
-                <div className='popup-btns'>
-                    <button className='save-btn' onClick={handleClick}>
-                        <span> Save</span>
-                    </button>
-                   
-                    <img src={close} onClick={closeModal}></img>
-                    
-                </div>
-            </div>
-            <div className='image-upload'>
-           
-            {selectedImage ?
-            <img 
-            className='selected-img'
-              src={URL.createObjectURL(selectedImage)}
+            <img src={close} onClick={props.closeModal}></img>
 
-            />:<img src={Rectangle}></img>
+          </div>
+        </div>
+        <div className='image-upload'>
+
+          {props.selectedImage ?
+            <img
+              className='selected-img'
+              src={URL.createObjectURL(props.selectedImage)}
+
+            /> : <img src={Rectangle}></img>
           }
-          <input className='hide' accept="image/*" type="file" onChange={handleImage} ref={fileInputRef}/>
-            <div className='upload' onClick={(event)=>{
-                event.preventDefault();
-                fileInputRef.current.click();
-            }}>
+          <input className='hide' accept="image/*" type="file" name="photo" onChange={props.changeImage} ref={fileInputRef} />
+          <div className='upload' onClick={(event) => {
+            event.preventDefault();
+            fileInputRef.current.click();
+          }}>
             <img src={upload}></img>
-          
 
-            </div>
 
-            </div>
-            <form>
-            <div className='admin-name'>
+          </div>
+
+        </div>
+        <form onSubmit={props.submit}>
+          <div className='admin-name'>
             <img src={profile}></img>
-                <input type="text" placeholder="Name"  ref={inputName} ></input>
-            </div>
-            <div className='admin-email' >
+            <input type="text" placeholder="Name" name='firstName' onChange={props.handleform} ></input>
+          </div>
+          <div className='admin-email' >
             <img src={sms}></img>
             <input type="email" placeholder="Email address " ></input>
-            </div>
-            <div className='admin-linkedin'>
+          </div>
+          <div className='admin-linkedin'>
             <img src={link}></img>
-            <input type="url" placeholder="LinkedIn URL (optional)" ref={inputUrl}  ></input>
-            </div>
-            </form>
+            <input type="url" placeholder="LinkedIn URL (optional)" name='url' onChange={props.handleform} ></input>
+          </div>
+        </form>
 
-        </div>
-        </div>
-       
-    )
+      </div>
+    </div>
+
+  )
 }
 export default TeamPage;

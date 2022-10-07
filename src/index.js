b91e969de5b4c9ccddee8ee313ab1457fca7b898
Amr Mohamed <amrmohamedyonis@gmail.com>
2022-10-07 22:34:59 +0200

    ready to merge

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
 import App from "./App";
+import { looksLikeNumber } from "yargs-parser";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App></App>);

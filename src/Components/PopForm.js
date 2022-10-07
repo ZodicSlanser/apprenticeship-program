c43196a18b712d1a394fdec43486a492935b3985
rroshdy11 <rroshdy75@gmail.com>
2022-10-07 05:25:02 +0200

    Required Skills and Role Menues are done with the Search

--- src/Components/PopForm.js
+++ src/Components/PopForm.js
@@ -1,23 +1,59 @@
 import React, { useState } from 'react'
 import '../styles/PopForm.css'
-import userFrame from '../icons/frame.png'
+import medalIcon from '../icons/medal-star.png'
 import clockIcon from '../icons/clock.png'
 import closeIcon from '../icons/close.png'
-import downarrw from '../icons/arrow-down.png'
-import DropMenu from './DropMenu'
+import DropDownRoleMenu  from './DropDownRoleMenu'
+import DropDownSkillsMenu from './DropDownSkillsMenu'
+import closeSquare from '../icons/close-square.png'
+
 export default function PopForm(){
+    /* Role*/
     const [role,setRole]=useState('Select Role')
-    const [dropmenu,setshow]=useState(true)
-    function pickRole(rolesent){
-        setRole(rolesent)
-        setshow(!dropmenu)
+    function pickRole(selectedRole){
+        setRole(selectedRole)
+        setshowRoleMenu(!showRoleMenu)
 
     }
-   
-    function roleDrop(){
-        setshow(!dropmenu)
+    /*To Toggle Role DropDown Menu */
+    const [showRoleMenu,setshowRoleMenu]=useState(false)
+    function toggleRoleMenu(){
+        setshowRoleMenu(!showRoleMenu)
     }
-
+    /******************************************************************* */
+    /*Role Description triggered by onChange*/
+    const [desc,setDesc]=useState('')
+    function saveDesc(event){
+        setDesc(event.target.value)
+    }
+    //**************************************************************** */
+    /*toggle the menu */
+    const [showRSkillsMenu,setshowRSkillsMenu]=useState(false)
+    function toggleSkillsMenu(){
+        setshowRSkillsMenu(!showRSkillsMenu)
+    }
+    const [reqSkills,setreqSkills]=useState([])
+    function appendToSkills(skill){
+        setreqSkills(prevSkills=>{
+            if(prevSkills.length<=2){
+            return[
+                ...prevSkills,
+                skill
+            ]
+        }
+        else
+        {
+            return prevSkills
+        }
+    })
+    }
+    function deleteSkill(index){
+        setreqSkills(reqSkills=>[
+              ...reqSkills.slice(0, index),
+              ...reqSkills.slice(index + 1, reqSkills.length)
+            ])
+  
+    }
     
     return(
         <div className="popup">
@@ -28,30 +64,42 @@ export default function PopForm(){
                     <img src={closeIcon} alt='Close utton'/>
                 </div>
             </div>
-            <div className='dropDown-wrapper'>
-                <img src={userFrame} alt='User Icon' className='user-Icon'/>
-                <div className='select-btn' onClick={roleDrop} >
-                        <div>{role}</div>
-                        <img src={downarrw} alt='arrow down' className='down-arrow'/>
-                </div>
-                {dropmenu && 
-                <div className='menu-content'>
-                        <div className='search' >
-                            <input type="text" placeholder='Search' />
-                        </div>
-                    <ul className="options">
-                        <li onClick={()=>pickRole('ios Developer') }>ios Developer</li>
-                        <li onClick={()=>pickRole('Android Developer') }>Android Developer</li>
-                        <li onClick={()=>pickRole('Full Stack Developer') }>Full Stack Developer</li>
-                        <li onClick={()=>pickRole('Front-end Developer') }>Front-end Developer</li>
-                        <li onClick={()=>pickRole('Back-end Developer') }>Back-end Developer</li>
-                    </ul>
-                </div> }
-    
-            </div>
+            <DropDownRoleMenu 
+                title={role}
+                showRoleMenu={showRoleMenu}
+                toggleRoleMenu={toggleRoleMenu}
+                handleChange={pickRole}
+                options={["ios Developer","Mobile Developer",
+                "Full Stack Developer","Front-end Developer","Back-end Developer"
+                ]}
+             />
             <div className='role-desc'>
                 <h3>Role Description</h3>
-                <input type="text" className='input-desc' placeholder="Description" />
+                <textarea type="text" className='input-desc' placeholder="Description" onChange={saveDesc}/>
+            </div>
+            <div className='req-skills-component'>
+                <h3>Required Skills (Select any 3)</h3>
+                 <DropDownSkillsMenu 
+                       Frame={medalIcon}
+                       showMenu={showRSkillsMenu}
+                       toggleMenu={toggleSkillsMenu}
+                       skills={["Swift","ios",
+                       "Objective-c","ARM"
+                       ]}
+                       chooseSkill={appendToSkills}
+                       chosenSkills={reqSkills}
+                 />
+                 <div className='reqSkills'>
+                    {
+                        reqSkills.map((skill,index)=>(
+                            <span key={index} className='singleSkill'>
+                                {skill}
+                                <img src={closeSquare} alt='close Icon' className='close-skill' onClick={()=>deleteSkill(index)}/>
+                            </span>
+                        ))
+                    }
+                 </div>
+
             </div>
             <div className='minimum-hours'>
                 <h3>Minimum Hours</h3>
@@ -59,9 +107,7 @@ export default function PopForm(){
                     <img src={clockIcon} alt='Clock Icon' className='clock-Icon'/>
                     <input type="text" className='input-hours' placeholder="No. of hours" />
                 </div>
-                
             </div>
-
 
         </div>
     )

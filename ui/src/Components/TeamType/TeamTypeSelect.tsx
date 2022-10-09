44cb8429cb80894ed48d23047017325c5c86cc46
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-10-09 20:53:47 +0200

    modified project structure

--- ui/src/Components/TeamType/TeamTypeSelect.tsx
+++ ui/src/Components/TeamType/TeamTypeSelect.tsx
@@ -1,24 +1,34 @@
-import React, { FC } from 'react'
-import styles from './TeamTypePanel.module.css'
-import untickCircle from '../../Assets/TeamType/untick-circle.svg';
-import tickCircle from '../../Assets/TeamType/tick-circle.svg';
+import React, { FC } from "react";
+import styles from "./TeamTypePanel.module.css";
+import untickCircle from "../../Assets/untick-circle.svg";
+import tickCircle from "../../Assets/tick-circle.svg";
 interface TeamTypeSelectProps {
-    teamType: string,
-    logo: string,
-    select: () => void,
-    isSelected: boolean,
+  teamType: string;
+  logo: string;
+  select: () => void;
+  isSelected: boolean;
 }
-export const TeamTypeSelect: FC<TeamTypeSelectProps> = ({ teamType, logo, select, isSelected }) => {
-    return (
-        <div className={styles.column}>
-            <div className={isSelected ? styles['team-type-selected'] : styles['type-button']} onClick={select} >     
-            <div className={styles.buttonInfo}>
-                <img src={logo} alt="logo" />
-                {teamType}
-            </div>
-                <img src={isSelected ? tickCircle : untickCircle} alt="tick-circle" />
-            </div>
+export const TeamTypeSelect: FC<TeamTypeSelectProps> = ({
+  teamType,
+  logo,
+  select,
+  isSelected,
+}) => {
+  return (
+    <div className={styles.column}>
+      <div
+        className={
+          isSelected ? styles["team-type-selected"] : styles["type-button"]
+        }
+        onClick={select}
+      >
+        <div className={styles.buttonInfo}>
+          <img src={logo} alt="logo" />
+          {teamType}
         </div>
-    )
-}
-export default TeamTypeSelect
+        <img src={isSelected ? tickCircle : untickCircle} alt="tick-circle" />
+      </div>
+    </div>
+  );
+};
+export default TeamTypeSelect;

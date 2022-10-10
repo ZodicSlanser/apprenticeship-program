d54f1385f4d6ee9e16e84fbed2cbcf088cbc1201
rroshdy11 <rroshdy75@gmail.com>
2022-10-10 18:35:53 +0200

    Modifying the Structure

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
 import "./App.css";
 function App() {}
 export default App;
+=======
+import React, { useState } from 'react'
+import PopForm from './Components/Roles/PopForm';
+import TeamRoles from './Components/Roles/TeamRoles';
+
+function App() {
+    return ( 
+      <>
+      <PopForm />
+    </>
+    )
+}
+export default App;
+>>>>>>> 56b79681 (Modifying the Structure)
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+<<<<<<< HEAD
 import App from "./App";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(<App></App>);
+=======
+import "./styles.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App></App>);
+>>>>>>> 56b79681 (Modifying the Structure)

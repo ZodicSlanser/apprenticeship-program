ad60611a52d47a5d485be14d6dfee670e973d148
IrrationalInteger <amrmohamedyonis@gmail.com>
2022-10-10 20:25:09 +0200

    Fixed Strucutre

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./App.tsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App></App>);

ce453291bcc8176bb01928e885dafe6f9061e447
rroshdy11 <rroshdy75@gmail.com>
2022-10-10 19:13:13 +0200

    Rename Styles.css to index.css

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 function App() {}
 export default App;
-=======
-import React, { useState } from 'react'
-import PopForm from './Components/Roles/PopForm';
-import TeamRoles from './Components/Roles/TeamRoles';
-
-function App() {
-    return ( 
-      <>
-      {/* <PopForm /> */}
-      <TeamRoles />
-    </>
-    )
-}
-export default App;
->>>>>>> 56b79681 (Modifying the Structure)

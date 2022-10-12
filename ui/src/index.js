a9439692cf5a65e8a8c8d99afd227dda08405b19
IrrationalInteger <amrmohamedyonis@gmail.com>
2022-10-12 21:30:34 +0200

    Progress on mainpage

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import MainPage from "./Components/MainPage/MainPage";
 ReactDOM.createRoot(document.getElementById("root")).render(<MainPage />);

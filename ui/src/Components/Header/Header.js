8731f82503e67d0226c633197369be7e94b28ad8
IrrationalInteger <amrmohamedyonis@gmail.com>
2022-10-15 02:58:01 +0200

    Preparing for rebase

---
+++ ui/src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import arrowLeft from "../../Assets/Header/arrow-left.svg";
+import add from "../../Assets/Header/add.svg";
+import "./Header.css";
+export default function Header() {
+  return (
+    <div className="FlexContainer">
+      <div className="back" onClick={() => console.log("Back")}>
+        <img src={arrowLeft} alt="arrow left" />
+        Back
+      </div>
+      <div className="title">Creating Apprenticeship</div>
+      <div className="publish">
+        <button
+          className="publish-button"
+          onClick={() => console.log("Published!")}
+        >
+          <img src={add} alt="add" />
+          Publish Apprenticeship
+        </button>
+      </div>
+    </div>
+  );
+}

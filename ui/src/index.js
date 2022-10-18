267ff640f44dce4473ea8881103b562bd88f890e
IrrationalInteger <99425163+IrrationalInteger@users.noreply.github.com>
2022-10-18 02:24:35 +0200

    Revert "Rename Styles.css to index.css"
    This reverts commit 176cc612e4ca3428399257e3684b5edc37022fde.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App> </App>);
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")).render(<App></App>);

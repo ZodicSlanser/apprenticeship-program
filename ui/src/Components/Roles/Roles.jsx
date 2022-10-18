db1f6f868776db90eb66abc19c9547f1d0bcf1d4
IrrationalInteger <99425163+IrrationalInteger@users.noreply.github.com>
2022-10-18 02:24:35 +0200

    Revert "Edit PopUpForm Added"
    This reverts commit 37046c8e7dd630cecd100beb77daf0bd910ba88c.

--- ui/src/Components/Roles/Roles.jsx
+++ ui/src/Components/Roles/Roles.jsx
@@ -4,71 +4,42 @@ import pen_icon from "../../Assets/Roles/pen-tool-2.png";
 import edit_icon from "../../Assets/Roles/edit.png";
 import copy_icon from "../../Assets/Roles/copy.png";
 import trash_icon from "../../Assets/Roles/trash.png";
-import PopFormUpdate from "./PopFormUpdate";
 export default function Roles(props) {
-  const [showPopFormUpdate, setShowPopFormUpdate] = useState(false);
-
-  function togglePopFormUpdate() {
-    setShowPopFormUpdate(!showPopFormUpdate);
-  }
-  function UpdateRoleAndToggle(Role, index) {
-    togglePopFormUpdate();
-    props.UpdateRole(Role, index);
-  }
   return (
-    <>
-      <div className="new-role">
-        <header>
-          <div className="role-head">
-            <div className="role-name">
-              <img src={pen_icon} alt="pen-icon" />
-              {/* add {props.name} */}
-              <h1>{props.type}</h1>
-            </div>
-            <div className="side-icons">
-              <img
-                src={edit_icon}
-                alt="edit_icon"
-                onClick={togglePopFormUpdate}
-              />
-              <img src={copy_icon} alt="copy_icon" />
-              <img
-                src={trash_icon}
-                alt="trash_icon"
-                onClick={() => props.handleDelete(props.index)}
-              />
-            </div>
+    <div className="new-role">
+      <header>
+        <div className="role-head">
+          <div className="role-name">
+            <img src={pen_icon} alt="pen-icon" />
+            {/* add {props.name} */}
+            <h1>{props.type}</h1>
           </div>
-          <div className="DescritionSpace">
-            <p>{props.desc}</p>
+          <div className="side-icons">
+            <img src={edit_icon} alt="edit_icon" />
+            <img src={copy_icon} alt="copy_icon" />
+            <img
+              src={trash_icon}
+              alt="trash_icon"
+              onClick={() => props.handleDelete(props.index)}
+            />
           </div>
-        </header>
-        <section className="role-skills">
-          {props.reqSkills.map((skill, index) => (
-            <div key={index} className="skill-req">
-              {skill}
-            </div>
-          ))}
-          {props.compSkills.map((skill, index) => (
-            <div key={index} className="skill-comp">
-              {skill}
-            </div>
-          ))}
-        </section>
-      </div>
-      {showPopFormUpdate && (
-        <PopFormUpdate
-          index={props.index}
-          type={props.type}
-          reqSkills={props.reqSkills}
-          compSkills={props.compSkills}
-          location={props.location}
-          hours={props.hours}
-          desc={props.desc}
-          togglePopForm={togglePopFormUpdate}
-          handleClick={UpdateRoleAndToggle}
-        />
-      )}
-    </>
+        </div>
+        <div className="DescritionSpace">
+          <p>{props.desc}</p>
+        </div>
+      </header>
+      <section className="role-skills">
+        {props.reqSkills.map((skill, index) => (
+          <div key={index} className="skill-req">
+            {skill}
+          </div>
+        ))}
+        {props.compSkills.map((skill, index) => (
+          <div key={index} className="skill-comp">
+            {skill}
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }

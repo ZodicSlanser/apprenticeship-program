bc66662d98447b240155bbd3da30e9b71ce062d0
IrrationalInteger <amrmohamedyonis@gmail.com>
2022-10-18 02:02:00 +0200

    Prepare for rebase

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 function App() {}
-=======
-import React, { useState } from "react";
-import PopForm from "./Components/Roles/PopForm";
-import TeamRoles from "./Components/Roles/TeamRoles";
-import Roles from "./Components/Roles/Roles";
-
-function App() {
-  return (
-    <div className="asd">
-      <TeamRoles />
-    </div>
-  );
-}
->>>>>>> 2c871440 (Integrating the popform and the delete role button)
 export default App;

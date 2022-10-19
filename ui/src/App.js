08d293028e2382d61ecdbf6c463f60fea7eeea2f
Nadda <82404860+Nadda12@users.noreply.github.com>
2022-10-19 04:53:21 +0200

    Company Description

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,3 +1,22 @@
-import "./App.css";
-function App() {}
-export default App;
+import React, { useState } from 'react'
+import Scaffolding from"./Components/CompanyDesc/Scaffolding"
+import CompanyDesc from "./Components/CompanyDesc/CompanyDesc"
+
+ 
+function App() {
+  
+  const components=[      
+   <CompanyDesc
+     panel= {<CompanyDesc />}
+   />
+
+  ]
+   return ( 
+     <>
+     <Scaffolding
+     children={components}
+     />
+   </>
+   )
+}
+export default App

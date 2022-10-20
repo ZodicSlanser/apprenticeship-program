a332ad909be8418885a908ceb5aed4ed3b7796c9
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-10-20 23:22:51 +0200

    Fixed CORS error, endpoints now functional

--- backend/App.js
+++ backend/App.js
@@ -7,12 +7,17 @@ import {
   AddValue,
   DeleteField,
 } from "./CRUDS/CRUD.js";
+import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
 const port = process.env.PORT || 9000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cors({
+  methods : ['GET','POST','PUT','DELETE','OPTIONS'],}
+))
 
 app.post("/add", (req, res) => {
   console.log("receiving data ...");
@@ -37,9 +42,10 @@ app.get("/view-all", (req, res) => {
 });
 //res.send(`${msg}`);
 //delete apprenticeship
-app.post("/delete", (req, res) => {
+app.delete("/delete", (req, res) => {
   console.log("receiving data ...");
-  const msg = DeleteApprenticeship(req.body.id);
+  console.log(req.body?.id)
+  const msg = DeleteApprenticeship(req.body?.id);
   res.send(`${msg}`);
 });
 //update apprenticeship

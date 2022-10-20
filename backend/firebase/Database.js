e5b71aa339bbb294211e4f9294b797c565913719
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-10-20 14:49:10 +0200

    fixed the path variable to secrets

--- backend/firebase/Database.js
+++ backend/firebase/Database.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const serviceAccount = require("../../secerts/privateKey.json");
+const serviceAccount = require("../secrets/privateKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

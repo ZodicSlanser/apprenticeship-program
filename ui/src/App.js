3baf61cb4b7835c59d63133e11f090e16300fd19
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-10-20 23:26:32 +0200

    Returned to original state

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage";
 import MainPage from "./Components/MainPage/MainPage";
@@ -14,16 +13,5 @@ function App() {
     </BrowserRouter>
   );
 }
-=======
-import {viewAllApprenticeships,deleteApprenticeship} from "./API interface/API";
 
-function App() {
-    //viewAllApprenticeships();
-    deleteApprenticeship("G5mAHTCaGBxO3fPNXXpC")
-    return (<div>  </div>);}
->>>>>>> 510b6f57 (Beta endpoints)
-=======
-function App() {
-    return (<div> </div>);}
->>>>>>> 266c12f9 (Returned to original state)
 export default App;

3b71021ad36818e43e8d24f693c2c3318add2381
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-10-28 04:11:55 +0200

    More refactors for a more readable code

--- ui/src/API interface/API.js
+++ ui/src/API interface/API.js
@@ -1,15 +1,5 @@
 import axios from "axios";
-const API_ERROR = 400;
-const API_CONFIG = {hostname : "localhost", PORT : 9000, scheme : "http" }
-const API_URL = `${API_CONFIG.scheme}://${API_CONFIG.hostname}:${API_CONFIG.PORT}`;
-const API_ROUTES = {
-  viewApprenticeship: `${API_URL}/view`,
-  addApprenticeship : `${API_URL}/add`,
-  viewAllApprenticeships : `${API_URL}/view-all`,
-  deleteApprenticeship : `${API_URL}/delete`,
-  updateApprenticeship : `${API_URL}/update`,
-  addValue : `${API_URL}/add-value`
-}
+import {API_ROUTES} from "./API_ROUTES";
 
 //adds the apprenticeship object to the Database and assigns an ID to it
 

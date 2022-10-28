bc6c0691b0e62c871a003a2e904a49bc8d6cb884
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-10-28 03:25:04 +0200

    added an example for the "UseState", "UseEffect"

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,14 @@
 import "./App.css";
+import {useEffect, useState} from "react";
+import {sandBoxViewAll,viewApprenticeship} from "./API interface/API";
 function App() {
+  const [Apprenticeships, setApprenticeships] = useState('');
+  useEffect(() => {
+    viewApprenticeship("okwhhEHmcisWUAuC3Aji",console.log);
+  }, []);
+
+  console.log(Apprenticeships);
+
   return <div> </div>;
 }
 export default App;

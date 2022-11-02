38ea38b29dea874a006c60f543fae74393d06ad0
IrrationalInteger <amrmohamedyonis@gmail.com>
2022-11-02 20:52:29 +0200

    Various responsiveness fixes

--- ui/src/Components/AprenticeshipDescription/ApprenticeshipDesc.js
+++ ui/src/Components/AprenticeshipDescription/ApprenticeshipDesc.js
@@ -1,18 +1,69 @@
-import React, { useState } from 'react'
-import ApprenticeshipDesc from './ApprenticeshipDesc.css'
-import exclmark from '../../Assets/ApprenticeshipDescription/exclmark.svg'
+import { useState } from "react";
+import "./ApprenticeshipDesc.css";
+import exclmark from "../../Assets/ApprenticeshipDescription/exclmark.svg";
+import TextareaAutosize from "react-textarea-autosize";
 
+export default function ApprenticeshipDesc(props) {
+  const [opacity, setOpacity] = useState(0.3);
+  const [typing, setTyping] = useState(false);
+  const [active, setActive] = useState(false);
 
-export default function (){
-    return(
-
-        <div className="panel">
-           <img src={exclmark} alt="exmark" className='exmark' /> 
-          <div className='panel-heading'>Apprenticeship Description</div>
-          <br/>
-          <div contentEditable className='textarea'>Enter Description</div>
-</div>
-
-)
-
+  return (
+    <div>
+      <div
+        className="panel"
+        onClick={() => {
+          setActive(true);
+          props.invokeActivity(null, 1);
+        }}
+        onMouseEnter={() => {
+          props.invokeActivity(null, 1);
+          setActive(true);
+        }}
+        onMouseLeave={() => {
+          if (!typing) {
+            setActive(false);
+            props.invokeActivity(null, 0);
+          } else props.invokeActivity(null, 0, true);
+        }}
+        style={
+          active
+            ? {
+                border: "1px solid #793EF5",
+                boxShadow: "0px 24px 34px rgba(0, 0, 0, 0.12)",
+              }
+            : null
+        }
+      >
+        <img src={exclmark} alt="exmark" className="exmark" />
+        <div className="panel-heading">ApprenticeshipDescription</div>
+        <br />
+        <div>
+          <TextareaAutosize
+            className="textarea"
+            placeholder={"Enter Description"}
+            onBlur={(e) => {
+              setActive(false);
+              props.setApprenticeshipDesc(e.target.value);
+              if (e.target.value !== "") {
+                setOpacity(1);
+                props.invokeApprenticeship(null, true, 2);
+              } else {
+                setOpacity(0.3);
+                props.invokeApprenticeship(null, false, 2);
+              }
+              props.invokeActivity(null, 0, false);
+              setTyping(false);
+            }}
+            onClick={() => {
+              setOpacity(1);
+              props.invokeActivity(null, 1, true);
+              setTyping(true);
+            }}
+            style={{ opacity: opacity }}
+          ></TextareaAutosize>
+        </div>
+      </div>
+    </div>
+  );
 }
--- ui/src/Components/CompanyDesc/CompanyDesc.js
+++ ui/src/Components/CompanyDesc/CompanyDesc.js
@@ -1,15 +1,69 @@
-import React, { useState } from 'react'
-import CompanyDesc from './CompanyDesc.css'
-import exclmark from '../../Assets/CompanyDesc/exclmark.svg'
+import { useState } from "react";
+import "./CompanyDesc.css";
+import exclmark from "../../Assets/CompanyDesc/exclmark.svg";
+import TextareaAutosize from "react-textarea-autosize";
 
-export default function (){
-    return(
-        <div className="panel">
-           <img src={exclmark} alt="exmark" className='exmark' /> 
-          <div className='panel-heading'>Company Description</div>
-          <br/>
-          <div contentEditable className='textarea' > Enter Description </div>
+export default function CompanyDesc(props) {
+  const [opacity, setOpacity] = useState(0.3);
+  const [typing, setTyping] = useState(false);
+  const [active, setActive] = useState(false);
 
-</div>
-)
-} 
+  return (
+    <div>
+      <div
+        className="panel"
+        onClick={() => {
+          setActive(true);
+          props.invokeActivity(null, 1);
+        }}
+        onMouseEnter={() => {
+          props.invokeActivity(null, 1);
+          setActive(true);
+        }}
+        onMouseLeave={() => {
+          if (!typing) {
+            setActive(false);
+            props.invokeActivity(null, 0);
+          } else props.invokeActivity(null, 0, true);
+        }}
+        style={
+          active
+            ? {
+                border: "1px solid #793EF5",
+                boxShadow: "0px 24px 34px rgba(0, 0, 0, 0.12)",
+              }
+            : null
+        }
+      >
+        <img src={exclmark} alt="exmark" className="exmark" />
+        <div className="panel-heading">Company Description</div>
+        <br />
+        <div>
+          <TextareaAutosize
+            className="textarea"
+            placeholder={"Enter Description"}
+            onBlur={(e) => {
+              setActive(false);
+              props.setCompanyDesc(e.target.value);
+              if (e.target.value !== "") {
+                setOpacity(1);
+                props.invokeCompany(null, true, 1);
+              } else {
+                setOpacity(0.3);
+                props.invokeCompany(null, false, 1);
+              }
+              props.invokeActivity(null, 0, false);
+              setTyping(false);
+            }}
+            onClick={() => {
+              setOpacity(1);
+              props.invokeActivity(null, 1, true);
+              setTyping(true);
+            }}
+            style={{ opacity: opacity }}
+          ></TextareaAutosize>
+        </div>
+      </div>
+    </div>
+  );
+}
